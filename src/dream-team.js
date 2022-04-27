3da828d500b30bbcadd391ed679c518871d53be6
ALena Demina <w.demina.alena@gmail.com>
2022-04-28 01:42:43 +0400

    feat: add dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let teamName = []
+  if (members != null) {
+    for (let i = 0; i < members.length; i++) {
+      if (members[i] != 0 && typeof members[i] === 'string') {
+        teamName.push(members[i].trim()[0])
+      }
+    }
+  }
+  result = teamName.join('').toUpperCase()
+  return result.split('').sort().join('')
 }
 
 module.exports = {
